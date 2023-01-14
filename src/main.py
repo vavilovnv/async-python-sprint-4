ec50c228d19875053e9b92829402ba89581edc45
nike <vavilovnv@gmail.com>
2023-01-14 14:59:32 +0300

    Start app added.

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from api import links
+from core.config import app_settings
+
+app = FastAPI(
+    title=app_settings.app_title,
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse,
+    redoc_url=None
+)
+
+app.include_router(links.router, prefix='/api/v1', tags=['Links'])
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host=app_settings.project_host,
+        port=app_settings.project_port,
+    )

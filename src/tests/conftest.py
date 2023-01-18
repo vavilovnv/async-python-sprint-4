81af1acf4824cd85fffd3ec5a68ffdd3091f2a83
nike <vavilovnv@gmail.com>
2023-01-18 22:22:50 +0300

    Tests updated and extended.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,65 +1,61 @@
 import asyncio
-import asyncpg
-
-import pytest
-import pytest_asyncio
-
+from asyncio import AbstractEventLoop
 from typing import AsyncGenerator, Generator
 
+import asyncpg
+import pytest
+import pytest_asyncio
 from httpx import AsyncClient
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-
-from db import Base, get_session
+from db import get_session
 from main import app
+from models.links import Link
+
+from .utils import TEST_DB_NAME, database_dsn
 
 
-BASE_URL = '127.0.0.1'
-TEST_DB_NAME = "db_tests"
-database_dsn = "postgresql+asyncpg://postgres:postgres@localhost:5432/db_tests"
+@pytest_asyncio.fixture(scope='function')
+async def client() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=app, follow_redirects=False,
+                           base_url='http://localhost') as async_client:
+        yield async_client
 
 
 @pytest.fixture(scope="session")
-def event_loop() -> Generator:
+def event_loop() -> Generator[AbstractEventLoop, None, None]:
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
 
 
-async def _create_test_db() -> None:
+async def create_test_db() -> None:
     user, password, database = 'postgres', 'postgres', TEST_DB_NAME
     try:
         await asyncpg.connect(database=database, user=user, password=password)
     except asyncpg.InvalidCatalogNameError:
         conn = await asyncpg.connect(database='postgres', user=user,
                                      password=password)
-        await conn.execute(f'CREATE DATABASE "{database}" OWNER "{user}"')
+        sql_command = text(f'CREATE DATABASE "{database}" '
+                           f'OWNER "{user}" ENCODING "utf8"')
+        await conn.execute(sql_command)
         await conn.close()
         await asyncpg.connect(database=database, user=user, password=password)
 
 
-@pytest_asyncio.fixture(scope='function')
-async def client() -> AsyncGenerator:
-    async with AsyncClient(app=app, follow_redirects=False,
-                           base_url=BASE_URL) as async_client:
-        yield async_client
-
-
 @pytest_asyncio.fixture(scope="module")
-async def async_session() -> AsyncGenerator:
-    await _create_test_db()
-    engine = create_async_engine(database_dsn, echo=True, future=True)
+async def async_session() -> AsyncGenerator[AsyncSession, None]:
+    await create_test_db()
+    engine = create_async_engine(database_dsn, echo=False, future=True)
     session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-    async with session() as s:
+        await conn.run_sync(Link.metadata.drop_all)
+        await conn.run_sync(Link.metadata.create_all)
+    async with session() as session:
         def get_session_override():
-            return s
+            return session
         app.dependency_overrides[get_session] = get_session_override
-        yield s
-
+        yield session
     await engine.dispose()
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,29 +1,95 @@
 import pytest
-
 from fastapi import status
 from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession
 
+from core.config import SHORT_URL_LENGTH
 from main import app
 
-
-COUNT_TESTS = 5
-LINKS = [f'http://test_{i}.py' for i in range(COUNT_TESTS)]
-ID_LINKS = {}
+from .utils import COUNT_LINKS, ID_LINKS, LINKS
 
 
-@pytest.mark.asyncio
-async def test_create_item(
-    client: AsyncClient, async_session: AsyncSession
-) -> None:
+@pytest.mark.asyncio()
+async def test_ping(client: AsyncClient) -> None:
+    response = await client.get(
+        app.url_path_for("check_db")
+    )
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert isinstance(data, dict)
+    assert 'ping_db' in data
+    assert data['ping_db'] > 0
+
+
+@pytest.mark.asyncio()
+async def test_create_short_url(client: AsyncClient) -> None:
     response = await client.post(
-        app.url_path_for('create_links'),
-        json=[{"original_link": LINKS[i]} for i in range(COUNT_TESTS)]
+        app.url_path_for("create_short_url"),
+        json={'original_url': LINKS[0]}
     )
     assert response.status_code == status.HTTP_201_CREATED
-    # assert isinstance(response.json(), list)
-    # for item in response.json():
-    #     assert 'id' in item, 'No field "id" in answer'
-    #     assert 'original_url' in item, 'No field "original_url" in answer'
-    #     ID_LINKS.update({item['id']: item['original_url']})
-    #     assert 'short_url' in item, 'No field "short_url" in answer'
+    data = response.json()
+    assert isinstance(data, dict)
+    assert 'url_id' in data, 'No field "id" in answer'
+    assert 'short_url' in data, 'No field "short_url" in answer'
+    assert len(data['url_id']) == SHORT_URL_LENGTH, 'Wrong length url_id'
+
+
+@pytest.mark.asyncio()
+async def test_create_short_urls(client: AsyncClient) -> None:
+    response = await client.post(
+        app.url_path_for("create_short_urls"),
+        json=[{'original_url': link} for link in LINKS]
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    data = response.json()
+    assert len(data) == COUNT_LINKS
+    assert isinstance(data, list), 'Response for create some links is not list'
+    for i, value in enumerate(data):
+        ID_LINKS[LINKS[i]] = value['url_id']
+
+
+@pytest.mark.asyncio()
+async def test_get_url(client: AsyncClient) -> None:
+    response = await client.get(app.url_path_for(
+        "get_url",
+        url_id=ID_LINKS[LINKS[0]])
+    )
+    assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
+    message = 'Original url not in header "location"'
+    assert LINKS[0] in response.headers.get('Location'), message
+
+
+@pytest.mark.asyncio()
+async def test_delete_url(client: AsyncClient) -> None:
+    original_url = LINKS[1]
+    await client.delete(
+        app.url_path_for("delete_short_url", url_id=ID_LINKS[original_url])
+    )
+    response = await client.get(
+        app.url_path_for("get_url", url_id=ID_LINKS[original_url]),
+        follow_redirects=True
+    )
+    assert response.status_code == status.HTTP_410_GONE
+
+
+@pytest.mark.asyncio()
+async def test_get_url_status(client: AsyncClient) -> None:
+    original_url = LINKS[0]
+    response = await client.get(
+        app.url_path_for("get_url_status", url_id=ID_LINKS[original_url])
+    )
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert isinstance(data, dict), 'Response is not dict'
+    assert data.get("usages_count")
+    assert data["usages_count"] == 1, 'Link usage is not equal 1'
+    response = await client.get(
+        app.url_path_for("get_url_status", url_id=ID_LINKS[original_url]),
+        params='full-info=yes'
+    )
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert isinstance(data, list), 'Response is not list'
+    assert len(data) == 1, 'Link usage is not equal 1'
+    assert data[0].get("client")
+    assert data[0].get("use_at")
